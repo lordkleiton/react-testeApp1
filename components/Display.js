4f1bbb1a0b84a6563f7c2c790c9c93b75a3f3c83
lordkleiton <m_s_r@outlook.com>
2019-06-30 23:02:32 -0300

    aplicando css basico

--- components/Display.js
+++ components/Display.js
@@ -1,5 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    title: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    container: {
+        width: '90%',
+        margin: 15,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 15,
+    },
+})
 
 export default class Blink extends Component {
     componentDidMount() {
@@ -7,8 +22,8 @@ export default class Blink extends Component {
 
     render() {
         return (
-            <View>
-                <Text>{this.props.film.title}</Text>
+            <View style={ styles.container }>
+                <Text style={ styles.title }>{this.props.film.title}</Text>
                 <Text>{this.props.film.description}</Text>
                 <Text>{this.props.film.director}</Text>
                 <Text>{this.props.film.producer}</Text>
