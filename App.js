cb7e51d5aa3e48e6bbc856f66369eca6672313fe
lordkleiton <m_s_r@outlook.com>
2019-06-30 22:15:23 -0300

    funcionando, mas com warning sobre state

--- App.js
+++ App.js
@@ -2,75 +2,60 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import Blink from './components/Display'
 
-
-
-async function hm(){
-	return new Promise((resolve, reject) => {
-		fetch('https://ghibliapi.herokuapp.com/films', { signal: this.abortController.signal })
-			.then(result => result.json())
-			.then((r) => {
-				resolve(r)
-			})
-			.catch(err => reject(err))
-	})
-}
-
-
-
 export default class BlinkApp extends Component {
+	_isMounted = false
+
 	state = {
-		data: []
+		data: [],
+		isLoading: true
 	}
 
 	abortController = new AbortController()
 
-	async hm() {
+	async getFilms() {
 		return new Promise((resolve, reject) => {
 			fetch('https://ghibliapi.herokuapp.com/films', { signal: this.abortController.signal })
-				.then(result => result.json())
-				.then((r) => {
-					resolve(r)
-				})
-				.catch(err => reject(err))
+				.then(r => r.json())
+				.then(r => resolve(r))
+				.catch(e => reject(e))
 		})
 	}
 
 	componentDidMount(){
-		this.hm().then((r) => this.setState({ data: r }))
+		this._isMounted = true
+		this.getFilms().then(r => {if (this._isMounted) this.setState({ data: r, isLoading: false })})
+	}
+
+	componentWillUnmount() {
+		//this.abortController.abort()
+		this._isMounted = false
+	}
+
+	default(){
+		return (
+			<View>
+				<Blink text='agora voce me vê' />
+			</View>
+		)
+	}
+
+	filmsList(){
+		let a = []
+
+		for (let i = 0; i < this.state.data.length; i++) {
+			a.push(<Blink text={this.state.data[i].title} key={i} />)
+		}
+
+		return (
+			<View>
+				{a}
+			</View>
+		)
 	}
 
 	render() {
-
-		if (this.state.data.length){
-
-			let a = []
-
-			for (let i = 0; i < this.state.data.length; i++){
-				a.push(<Blink text={this.state.data[i].title} key={i} />)
-			}
-			
-			return (
-				<View>
-					{a}
-				</View>
-			)
-		}
-		else{
-			return (
-				<View>
-					<Blink text='I love to blink' />
-					<Blink text='Yes blinking is so great' />
-					<Blink text='Why did they ever take this out of HTML' />
-					<Blink text='Look at me look at me look at me' />
-					<Blink text='cala a boca filho da puta' />
-				</View>
-			)
-		}
-
-		
+		return this.state.data.length ? this.filmsList() : this.default()
 	}
 
-	componentWillUnmount(){
-		this.abortController.abort()
-	}
+	
 }
